41c3810540917e3134b6e52f315a7f6f25b13a81
Lucaaureo <luluaureo@gmail.com>
2025-08-18 18:19:16 -0300

    Add files via upload
    Aula 3- exercicio 1

---
+++ 1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#define TAM 3
+
+void lerM( int M[TAM][TAM], char nome){
+    int i, j;
+    printf("digite os elementos da matriz %c (%dx%d):\n", nome, TAM, TAM);
+    for (i = 0; i<TAM; i++) {
+        for (j=0; j<TAM; j++){
+            scanf("%d", &M[i][j]);
+    }
+}
+}
+void imprimirM(int M[TAM][TAM], char nome){
+    int i;
+    int j;
+    printf("\nmatriz %c:\n", nome);
+    for (i=0; i<TAM; i++){
+        for (j=0; j<TAM; j++){
+            printf("%3d ", M[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+
+void somarM(int A[TAM][TAM], int B[TAM][TAM], int C[TAM][TAM]){
+    int i, j;
+    for (i=0; i<TAM; i++){
+        for (j=0; j<TAM; j++){
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
+
+void multiplicarM(int A[TAM][TAM], int B[TAM][TAM], int C[TAM][TAM]){
+    int i, j, k;
+    for (i = 0; i < TAM; i++) {
+        for (j = 0; j < TAM; j++) {
+            C[i][j] = 0; 
+            for (k=0; k < TAM; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }   
+    } 
+
+}
+ int main () {
+    int A[TAM][TAM], B[TAM][TAM], C[TAM][TAM], matrizProduto[TAM][TAM];
+    lerM(A, 'A');
+    lerM(B, 'B');
+
+    somarM(A, B, C);
+
+
+    imprimirM(A, 'A');
+    imprimirM(B, 'B');
+    imprimirM(C, 'C');
+    multiplicarM(A, B, matrizProduto);
+    imprimirM(matrizProduto, 'P');
+
+ return 0;   
+}
